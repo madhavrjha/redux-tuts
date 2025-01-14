8a345925f0bc6aa347b0e4d5be9166efd13ec698
Madhav  R. Jha <53477434+madhavrjha@users.noreply.github.com>
2025-01-14 16:35:49 +0530

    feat: add async user fetching and reducer for usersSlice

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,19 +1,30 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import axios from 'axios'
 
-const initialState = [
-	{ id: '0', name: 'Dude Lebowski' },
-	{ id: '1', name: 'Neil Young' },
-	{ id: '2', name: 'Dave Gray' },
-]
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users'
+
+const initialState = []
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+	try {
+		const response = await axios(USERS_URL)
+		return response.data
+	} catch (e) {
+		return e.message
+	}
+})
 
 const usersSlice = createSlice({
 	name: 'users',
 	initialState,
 	reducers: {},
+	extraReducers: builder => {
+		builder.addCase(fetchUsers.fulfilled, (state, action) => {
+			return action.payload
+		})
+	},
 })
 
 export const selectAllUsers = state => state.users
-
-// export const {} = usersSlice.actions
 
 export default usersSlice.reducer
