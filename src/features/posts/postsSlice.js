bb99d76e15c6c2b5c986a6f5072f037211391d7a
Madhav  R. Jha <53477434+madhavrjha@users.noreply.github.com>
2025-01-14 12:26:55 +0530

    feat: add date-fns library and display post timestamps

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit'
+import { sub } from 'date-fns'
 
 const initialState = [
 	{
@@ -6,12 +7,14 @@ const initialState = [
 		title: 'Learning Redux Toolkit',
 		content: "I've heard good things.",
 		userId: '1',
+		date: sub(new Date(), { minutes: 10 }).toISOString(),
 	},
 	{
 		id: '2',
 		title: 'Slices...',
 		content: 'The more I say slice, the more I want pizza.',
 		userId: '2',
+		date: sub(new Date(), { minutes: 5 }).toISOString(),
 	},
 ]
 
@@ -25,7 +28,7 @@ export const postsSlice = createSlice({
 			},
 			prepare: (title, content, userId) => {
 				return {
-					payload: { id: nanoid(), title, content, userId },
+					payload: { id: nanoid(), title, content, date: new Date().toISOString(), userId },
 				}
 			},
 		},
