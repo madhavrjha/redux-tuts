2e9a05002187df14a5914fe365f8f68c943ef3f2
Madhav  R. Jha <53477434+madhavrjha@users.noreply.github.com>
2025-01-16 12:56:18 +0530

    feat: Implement routing and layout for blog application

--- src/features/posts/SinglePostPage.jsx
+++ src/features/posts/SinglePostPage.jsx
@@ -3,10 +3,11 @@ import { selectPostById } from './postsSlice'
 import PostAuthor from './PostAuthor'
 import TimeAgo from './TimeAgo'
 import ReactionButtons from './ReactionButtons'
+import { useParams } from 'react-router-dom'
 
 const SinglePostPage = () => {
-	const postId = 1
-	const post = useSelector(state => selectPostById(state, postId))
+	const { postId } = useParams()
+	const post = useSelector(state => selectPostById(state, +postId))
 
 	if (!post) {
 		return (
@@ -21,7 +22,6 @@ const SinglePostPage = () => {
 			<h2>{post.title}</h2>
 			<p>{post.body}</p>
 			<p className='postCredit'>
-				{/* <Link to={`/post/edit/${post.id}`}>Edit Post</Link> */}
 				<PostAuthor userId={post.userId} />
 				<TimeAgo timestamp={post.date} />
 			</p>
