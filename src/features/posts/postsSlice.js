f8545dafbdda3ca7368a1f82cbb50e28cfa9bc17
Madhav  R. Jha <53477434+madhavrjha@users.noreply.github.com>
2025-01-20 14:31:45 +0530

    Refactor PostExcerpt component to use useSelector and selectPostById, update PostsList to use selectPostIds, and update postsSlice to use createEntityAdapter

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,15 +1,18 @@
-import { createAsyncThunk, createSelector, createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createEntityAdapter, createSelector, createSlice } from '@reduxjs/toolkit'
 import axios from 'axios'
 import { sub } from 'date-fns'
 
 const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts'
 
-const initialState = {
-	posts: [],
+const postsAdapter = createEntityAdapter({
+	sortComparer: (a, b) => b.date.localeCompare(a.date),
+})
+
+const initialState = postsAdapter.getInitialState({
 	status: 'idle', //'idle' | 'loading' | 'succeeded' | 'failed'
 	error: null,
 	count: 0,
-}
+})
 
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
 	try {
@@ -57,7 +60,7 @@ export const postsSlice = createSlice({
 	reducers: {
 		reactionAdded: (state, action) => {
 			const { postId, reaction } = action.payload
-			const existingPost = state.posts.find(p => p.id === postId)
+			const existingPost = state.entities[postId]
 			if (existingPost) {
 				existingPost.reactions[reaction]++
 			}
@@ -86,7 +89,7 @@ export const postsSlice = createSlice({
 					},
 				}))
 
-				state.posts = loadedPosts
+				postsAdapter.upsertMany(state, loadedPosts)
 			})
 			.addCase(fetchPosts.rejected, (state, action) => {
 				state.status = 'failed'
@@ -104,7 +107,7 @@ export const postsSlice = createSlice({
 						coffee: 0,
 					},
 				}
-				state.posts.push(addedPost)
+				postsAdapter.addOne(state, addedPost)
 			})
 			.addCase(updatePost.fulfilled, (state, action) => {
 				if (!action.payload?.id) {
@@ -113,10 +116,8 @@ export const postsSlice = createSlice({
 					return
 				}
 
-				const { id } = action.payload
 				action.payload.date = new Date().toISOString()
-				const posts = state.posts.filter(p => p.id !== id)
-				state.posts = [...posts, action.payload]
+				postsAdapter.upsertOne(state, action.payload)
 			})
 			.addCase(deletePost.fulfilled, (state, action) => {
 				if (!action.payload?.id) {
@@ -125,18 +126,20 @@ export const postsSlice = createSlice({
 					return
 				}
 				const { id } = action.payload
-				const posts = state.posts.filter(post => post.id !== id)
-				state.posts = posts
+				postsAdapter.removeOne(state, id)
 			})
 	},
 })
 
-export const selectAllPosts = state => state.posts.posts
+export const {
+	selectAll: selectAllPosts,
+	selectById: selectPostById,
+	selectIds: selectPostIds,
+} = postsAdapter.getSelectors(state => state.posts)
+
 export const getPostsStatus = state => state.posts.status
 export const getPostsError = state => state.posts.error
 export const getCount = state => state.posts.count
-
-export const selectPostById = (state, postId) => state.posts.posts.find(post => post.id === postId)
 
 export const selectPostsByUser = createSelector([selectAllPosts, (state, userId) => userId], (posts, userId) =>
 	posts.filter(post => post.userId === userId)
